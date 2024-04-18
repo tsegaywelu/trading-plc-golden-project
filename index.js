5d8cda1f4442a6ca8748ab08b8bfc1b86c17d28b
Tsegay Welu <tsegaygebrezgi56@gmail.com>
2024-04-18 15:23:30 +0300

    i am not shre how

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const express=require('express');
-const cors=require('cors');
-const app=express();
+const express = require("express");
+const cors = require("cors");
+const app = express();
 app.use(cors());
-const bodyParser=require('body-parser');
-const dotenv = require('dotenv')
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 dotenv.config();
-//to day is 15 for github ceck
-const fileUpload = require('express-fileupload');
-const port= process.env.port||3000;//if no port in .env file use 3000 
-app.use(bodyParser.urlencoded({extended:true}));///used to form data
+//to day is 15 for github ceck    no to day is 18
+const fileUpload = require("express-fileupload");
+const port = process.env.port || 3000; //if no port in .env file use 3000
+app.use(bodyParser.urlencoded({ extended: true })); ///used to form data
 app.use(bodyParser.json());
 app.use(fileUpload());
 
 //telling where is the file to express
-app.use('/public',express.static('uploads'));//this displays the image http://localhost:3000/public/up.jpg
+app.use("/public", express.static("uploads")); //this displays the image http://localhost:3000/public/up.jpg
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.mongodburl).then(()=>console.log('database connected'))
-.catch(err=>console.log(err))
-const route=require('./routes/route.js');
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.mongodburl)
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log(err));
+const route = require("./routes/route.js");
 app.use(route);
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
