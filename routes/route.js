9537d8fe7a3b636ab2ceffe74620580f95a51af2
Tsegay Welu <tsegaygebrezgi56@gmail.com>
2024-04-19 20:27:27 +0300

    by moke

--- routes/route.js
+++ routes/route.js
@@ -1,11 +1,11 @@
-const router=require('express').Router();
-const Newsadder = require('../Controllers/Uploadnews');
-const userauthentication=require('../Controllers/auth')
+const router = require("express").Router();
+const Newsadder = require("../Controllers/Uploadnews");
+const userauthentication = require("../Controllers/auth");
 
-router.post('/addnews',userauthentication,Newsadder.addnews);
-router.get('/getnews',Newsadder.getnews);
-router.post('/registeruser',Newsadder.Registeruser);
-router.post('/loginuser',Newsadder.loguser);
-router.post('/checktoken',Newsadder.checktoken);
+router.post("/addnews", userauthentication, Newsadder.addnews);
+router.get("/getnews", Newsadder.getnews);
+router.post("/registeruser", Newsadder.Registeruser);
+router.post("/loginuser", Newsadder.loguser);
+router.post("/checktoken", Newsadder.checktoken);
 
-module.exports=router;
+module.exports = router;
